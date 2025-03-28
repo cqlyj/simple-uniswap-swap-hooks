16b335c98e819a39a8d9bcfe4d8bc4d823dfa50b
cqlyj <luoyingjie0721@gmail.com>
2025-03-29 00:30:29 +0800

    feat: add CreatePoolAndAddLiquidity script for automated pool creation and liquidity addition

---
+++ script/CreatePoolAndAddLiquidity.s.sol
@@ -0,0 +1,179 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {Script, console} from "forge-std/Script.sol";
+import {PositionManager} from "v4-periphery/src/PositionManager.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {CurrencyLibrary, Currency} from "v4-core/src/types/Currency.sol";
+import {Actions} from "v4-periphery/src/libraries/Actions.sol";
+import {LiquidityAmounts} from "v4-core/test/utils/LiquidityAmounts.sol";
+import {TickMath} from "v4-core/src/libraries/TickMath.sol";
+import {IERC20} from "forge-std/interfaces/IERC20.sol";
+import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
+import {Vm} from "forge-std/Vm.sol";
+import {IAllowanceTransfer} from "permit2/src/interfaces/IAllowanceTransfer.sol";
+
+contract CreatePoolAndAddLiquidity is Script {
+    using CurrencyLibrary for Currency;
+
+    IERC20 constant token0 =
+        IERC20(address(0x779877A7B0D9E8603169DdbD7836e478b4624789)); // LINK
+    IERC20 constant token1 =
+        IERC20(address(0xFF34B3d4Aee8ddCd6F9AFFFB6Fe49bD371b8a357)); // DAI
+    Currency constant currency0 = Currency.wrap(address(token0));
+    Currency constant currency1 = Currency.wrap(address(token1));
+    address hookAddress =
+        Vm(address(vm)).getDeployment("OneTimeSwap", uint64(block.chainid));
+    IHooks hookContract = IHooks(address(hookAddress));
+    PositionManager constant posm =
+        PositionManager(
+            payable(address(0x429ba70129df741B2Ca2a85BC3A2a3328e5c09b4))
+        );
+    IAllowanceTransfer constant PERMIT2 =
+        IAllowanceTransfer(address(0x000000000022D473030F116dDEE9F6B43aC78BA3));
+
+    /////////////////////////////////////
+    // --- Parameters to Configure --- //
+    /////////////////////////////////////
+
+    // --- pool configuration --- //
+    // fees paid by swappers that accrue to liquidity providers
+    uint24 lpFee = 3000; // 0.30%
+    int24 tickSpacing = 60;
+
+    // starting price of the pool, in sqrtPriceX96
+    uint160 startingPrice = 79228162514264337593543950336; // floor(sqrt(1) * 2^96)
+
+    // --- liquidity position configuration --- //
+    uint256 public token0Amount = 1e18;
+    uint256 public token1Amount = 1e18;
+
+    // range of the position
+    int24 tickLower = -600; // must be a multiple of tickSpacing
+    int24 tickUpper = 600;
+
+    /////////////////////////////////////
+
+    function run() external {
+        // tokens should be sorted
+        PoolKey memory pool = PoolKey({
+            currency0: currency0, // LINK
+            currency1: currency1, // DAI
+            fee: lpFee,
+            tickSpacing: tickSpacing,
+            hooks: hookContract
+        });
+        bytes memory hookData = new bytes(0);
+
+        // --------------------------------- //
+
+        // Converts token amounts to liquidity units
+        uint128 liquidity = LiquidityAmounts.getLiquidityForAmounts(
+            startingPrice,
+            TickMath.getSqrtPriceAtTick(tickLower),
+            TickMath.getSqrtPriceAtTick(tickUpper),
+            token0Amount,
+            token1Amount
+        );
+
+        // slippage limits
+        uint256 amount0Max = token0Amount + 1 wei;
+        uint256 amount1Max = token1Amount + 1 wei;
+
+        (
+            bytes memory actions,
+            bytes[] memory mintParams
+        ) = _mintLiquidityParams(
+                pool,
+                tickLower,
+                tickUpper,
+                liquidity,
+                amount0Max,
+                amount1Max,
+                address(this),
+                hookData
+            );
+
+        // multicall parameters
+        bytes[] memory params = new bytes[](2);
+
+        // initialize pool
+        params[0] = abi.encodeWithSelector(
+            posm.initializePool.selector,
+            pool,
+            startingPrice,
+            hookData
+        );
+
+        // mint liquidity
+        params[1] = abi.encodeWithSelector(
+            posm.modifyLiquidities.selector,
+            abi.encode(actions, mintParams),
+            block.timestamp + 60
+        );
+
+        // if the pool is an ETH pair, native tokens are to be transferred
+        uint256 valueToPass = currency0.isAddressZero() ? amount0Max : 0;
+
+        vm.startBroadcast();
+        tokenApprovals();
+        vm.stopBroadcast();
+
+        // multicall to atomically create pool & add liquidity
+        vm.broadcast();
+        posm.multicall{value: valueToPass}(params);
+    }
+
+    /// @dev helper function for encoding mint liquidity operation
+    /// @dev does NOT encode SWEEP, developers should take care when minting liquidity on an ETH pair
+    function _mintLiquidityParams(
+        PoolKey memory poolKey,
+        int24 _tickLower,
+        int24 _tickUpper,
+        uint256 liquidity,
+        uint256 amount0Max,
+        uint256 amount1Max,
+        address recipient,
+        bytes memory hookData
+    ) internal pure returns (bytes memory, bytes[] memory) {
+        bytes memory actions = abi.encodePacked(
+            uint8(Actions.MINT_POSITION),
+            uint8(Actions.SETTLE_PAIR)
+        );
+
+        bytes[] memory params = new bytes[](2);
+        params[0] = abi.encode(
+            poolKey,
+            _tickLower,
+            _tickUpper,
+            liquidity,
+            amount0Max,
+            amount1Max,
+            recipient,
+            hookData
+        );
+        params[1] = abi.encode(poolKey.currency0, poolKey.currency1);
+        return (actions, params);
+    }
+
+    function tokenApprovals() public {
+        if (!currency0.isAddressZero()) {
+            token0.approve(address(PERMIT2), type(uint256).max);
+            PERMIT2.approve(
+                address(token0),
+                address(posm),
+                type(uint160).max,
+                type(uint48).max
+            );
+        }
+        if (!currency1.isAddressZero()) {
+            token1.approve(address(PERMIT2), type(uint256).max);
+            PERMIT2.approve(
+                address(token1),
+                address(posm),
+                type(uint160).max,
+                type(uint48).max
+            );
+        }
+    }
+}
